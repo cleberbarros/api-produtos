40f83a8c2e57104ae34fa09837ba4d3f127df11b
cleberbarrosBuilders@bitbucket.org <ATBBHTpjFwKDzqE4tfhhpQcGGYSrF3B874CF>
2023-08-19 08:38:53 -0300

    Colocando swagger

--- src/main/java/br/com/bernhoeft/gerenciadorprodutos/GerenciadorprodutosApplication.java
+++ src/main/java/br/com/bernhoeft/gerenciadorprodutos/GerenciadorprodutosApplication.java
@@ -1,9 +1,12 @@
 package br.com.bernhoeft.gerenciadorprodutos;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger API Produtos", version="1", description = "API Gerenciador de Produtos"))
 public class GerenciadorprodutosApplication {
 
 	public static void main(String[] args) {
